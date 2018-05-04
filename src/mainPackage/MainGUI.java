1ff3c4cde0e47a00576beec79c7f2f87fb139dda
Kevin Kammleiter <kevinkammleiter@gmail.com>
2018-05-05 01:48:15 +0200

    added new GUI

--- src/mainPackage/MainGUI.java
+++ src/mainPackage/MainGUI.java
@@ -1,32 +1,33 @@
 package mainPackage;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTable;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JMenu;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
 
 public class MainGUI extends JFrame {
 
 	private static final long serialVersionUID = 1085815654054173311L;
 	private JPanel contentPane;
-	private JTable table1;
-	private JTable table2;
+	private JPanel MainPanel;
+	private static JTabbedPane tabbedPane;
+	static JTable tableKunden;
+	static JTable tableKonten;
 
 	static String driver = "com.mysql.jdbc.Driver";
 	static String url = "jdbc:mysql://62.141.46.196/hft?useLegacyDatetimeCode=false&serverTimezone=UTC";
@@ -45,6 +46,12 @@ public class MainGUI extends JFrame {
 		}
 	}
 
+	static String[] kundenHeader = { "Kundennummer", "Nachname", "Vorname", "Strasse", "Hausnummer", "PLZ", "Ort" };
+	static Object[][] kundenData;
+
+	static String[] konteHeader = { "ID", "Kontonummer", "BLZ", "Kontoname", "Kontostand" };
+	static Object[][] kontenData;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -59,108 +66,151 @@ public class MainGUI extends JFrame {
 	}
 
 	public MainGUI() {
-		setTitle("Bankdatenbank");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1280, 720);
+		setBounds(100, 100, 890, 590);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		String[] columnNames1 = { "ID", "Kundennummer", "Nachname", "Vorname", "Strasse", "Hausnummer", "PLZ", "Ort" };
-		Object[][] data1 = new Object[rowCount("Kunden")][8];
-
-		int i = 0;
-
-		ResultSet rs;
-		ResultSetMetaData rsm;
-		try {
-			rs = s.executeQuery("SELECT * FROM Kunden");
-			rsm = rs.getMetaData();
-
-			while (rs.next()) {
-				for (int j = 1; j <= rsm.getColumnCount(); j++) {
-					data1[i][j - 1] = rs.getString(j);
-				}
-				i++;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(29, 41, 1200, 600);
-		contentPane.add(tabbedPane);
-
-		table1 = new JTable(data1, columnNames1);
-		tabbedPane.addTab("Kunden", null, table1, "Kundeninformationen");
-
-		String[] columnNames2 = { "ID", "Kontonummer", "BLZ", "Kontoname", "Kontostand" };
-		Object[][] data2 = new Object[rowCount("Kontoinformation")][5];
-
-		i = 0;
-
-		try {
-			rs = s.executeQuery("SELECT * FROM Kontoinformation");
-			rsm = rs.getMetaData();
-
-			while (rs.next()) {
-				for (int j = 1; j <= rsm.getColumnCount(); j++) {
-					data2[i][j - 1] = rs.getString(j);
-				}
-				i++;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		table2 = new JTable(data2, columnNames2);
-		tabbedPane.addTab("Konten", null, table2, "Kontoinformation");
+		setResizable(false);
 
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 2, 119, 26);
+		menuBar.setBounds(0, 0, 884, 21);
 		contentPane.add(menuBar);
 
-		JMenu mnManu = new JMenu("Menu");
-		menuBar.add(mnManu);
+		JMenu mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
 
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mntmExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
 			}
 		});
-		mnManu.add(mntmExit);
+		mnMenu.add(mntmExit);
 
 		JMenu mnUpdate = new JMenu("Update");
 		menuBar.add(mnUpdate);
 
 		JMenuItem mntmUpdateTables = new JMenuItem("Update Tables");
+		mntmUpdateTables.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
 		mnUpdate.add(mntmUpdateTables);
 
-		JMenu mnAbout = new JMenu("Help");
+		JMenu mnAbout = new JMenu("About");
 		menuBar.add(mnAbout);
 
 		JMenuItem mntmAbout = new JMenuItem("About");
 		mntmAbout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(contentPane, "Gurpal Sing, Kevin Kammleiter", "About",
+				JOptionPane.showMessageDialog(contentPane, "Gurpal Singh, Kevin Kammleiter", "About",
 						JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		mnAbout.add(mntmAbout);
+
+		MainPanel = new JPanel();
+		MainPanel.setBounds(0, 21, 884, 540);
+		contentPane.add(MainPanel);
+		MainPanel.setLayout(null);
+
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 884, 540);
+		MainPanel.add(tabbedPane);
+
+		updateTables();
+
 	}
 
-	protected static int rowCount(String table) {
-		ResultSet r;
-		int count = 0;
+	protected static void updateTables() {	
+		kundenData = getObjectData("Kunden", false);
+		kontenData = getObjectData("Kontoinformation", true);
+
+		JScrollPane scrollPaneKunden = new JScrollPane();
+		tabbedPane.addTab("Kunden", null, scrollPaneKunden, null);
+
+		JScrollPane scrollPaneKonten = new JScrollPane();
+		tabbedPane.addTab("Konten", null, scrollPaneKonten, null);
+
+		tableKunden = new JTable(kundenData, kundenHeader);
+		tableKunden.setEnabled(false);
+		scrollPaneKunden.setViewportView(tableKunden);
+
+		tableKonten = new JTable(kontenData, konteHeader);
+		tableKunden.setEnabled(false);
+		scrollPaneKonten.setViewportView(tableKonten);
+				
+		tableKunden.repaint();
+		tableKonten.repaint();
+	}
+
+	protected static Object[][] getObjectData(String table, boolean id) {
+		Object[][] end = new Object[getRowCount(table)][getColCount(table)];
+
+		int start = 2;
+
+		if (id) {
+			start = 1;
+		}
+
+		int i = 0;
+		ResultSet rs;
+		ResultSetMetaData rsm;
 		try {
-			r = s.executeQuery("SELECT COUNT(*) AS rowcount FROM Kunden");
-			r.next();
-			count = r.getInt("rowcount");
-			r.close();
+			rs = s.executeQuery("SELECT * FROM " + table);
+			rsm = rs.getMetaData();
+
+			while (rs.next()) {
+				for (int j = start; j <= rsm.getColumnCount(); j++) {
+					end[i][j - start] = rs.getString(j);
+				}
+				i++;
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+		return end;
+	}
+
+	protected static int getRowCount(String table) {
+		int count = 0;
+		ResultSet r = null;
+		try {
+			r = s.executeQuery("SELECT COUNT(*) AS rowcount FROM " + table);
+			r.next();
+			count = r.getInt("rowcount");
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} finally {
+			try {
+				r.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return count;
+	}
+
+	protected static int getColCount(String table) {
+		int count = 0;
+		ResultSet r = null;
+		ResultSetMetaData rsm;
+		try {
+			r = s.executeQuery("SELECT * FROM " + table);
+			rsm = r.getMetaData();
+			count = rsm.getColumnCount();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} finally {
+			try {
+				r.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
 		}
 		return count;
 	}
---
+++ src/mainPackage/TempOutdated.java
@@ -0,0 +1,167 @@
+package mainPackage;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JTabbedPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JMenu;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TempOutdated extends JFrame {
+
+	private static final long serialVersionUID = 1085815654054173311L;
+	private JPanel contentPane;
+	private JTable table1;
+	private JTable table2;
+
+	static String driver = "com.mysql.jdbc.Driver";
+	static String url = "jdbc:mysql://62.141.46.196/hft?useLegacyDatetimeCode=false&serverTimezone=UTC";
+	static String username = "hft";
+	static String password = "hft100hft";
+
+	static Connection c;
+	static Statement s;
+
+	static {
+		try {
+			c = DriverManager.getConnection(url, username, password);
+			s = c.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TempOutdated frame = new TempOutdated();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public TempOutdated() {
+		setTitle("Bankdatenbank");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		String[] columnNames1 = { "ID", "Kundennummer", "Nachname", "Vorname", "Strasse", "Hausnummer", "PLZ", "Ort" };
+		Object[][] data1 = new Object[rowCount("Kunden")][8];
+
+		int i = 0;
+
+		ResultSet rs;
+		ResultSetMetaData rsm;
+		try {
+			rs = s.executeQuery("SELECT * FROM Kunden");
+			rsm = rs.getMetaData();
+
+			while (rs.next()) {
+				for (int j = 1; j <= rsm.getColumnCount(); j++) {
+					data1[i][j - 1] = rs.getString(j);
+				}
+				i++;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(29, 41, 1200, 600);
+		contentPane.add(tabbedPane);
+
+		table1 = new JTable(data1, columnNames1);
+		tabbedPane.addTab("Kunden", null, table1, "Kundeninformationen");
+
+		String[] columnNames2 = { "ID", "Kontonummer", "BLZ", "Kontoname", "Kontostand" };
+		Object[][] data2 = new Object[rowCount("Kontoinformation")][5];
+
+		i = 0;
+
+		try {
+			rs = s.executeQuery("SELECT * FROM Kontoinformation");
+			rsm = rs.getMetaData();
+
+			while (rs.next()) {
+				for (int j = 1; j <= rsm.getColumnCount(); j++) {
+					data2[i][j - 1] = rs.getString(j);
+				}
+				i++;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		table2 = new JTable(data2, columnNames2);
+		tabbedPane.addTab("Konten", null, table2, "Kontoinformation");
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 2, 119, 26);
+		contentPane.add(menuBar);
+
+		JMenu mnManu = new JMenu("Menu");
+		menuBar.add(mnManu);
+
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		mnManu.add(mntmExit);
+
+		JMenu mnUpdate = new JMenu("Update");
+		menuBar.add(mnUpdate);
+
+		JMenuItem mntmUpdateTables = new JMenuItem("Update Tables");
+		mnUpdate.add(mntmUpdateTables);
+
+		JMenu mnAbout = new JMenu("Help");
+		menuBar.add(mnAbout);
+
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(contentPane, "Gurpal Sing, Kevin Kammleiter", "About",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		mnAbout.add(mntmAbout);
+	}
+
+	protected static int rowCount(String table) {
+		ResultSet r;
+		int count = 0;
+		try {
+			r = s.executeQuery("SELECT COUNT(*) AS rowcount FROM Kunden");
+			r.next();
+			count = r.getInt("rowcount");
+			r.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+}
