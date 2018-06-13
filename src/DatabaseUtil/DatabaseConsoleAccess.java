f07f39edc33e33df631dce8ee56808b1650d1cd3
unknown <kevinkammleiter@gmail.com>
2018-06-13 10:48:38 +0200

    fsf

--- src/DatabaseUtil/DatabaseConsoleAccess.java
+++ src/DatabaseUtil/DatabaseConsoleAccess.java
@@ -1,5 +1,12 @@
 package DatabaseUtil;
 
 public class DatabaseConsoleAccess {
+	
+	public static boolean createKunde(String vorname, String nachname) {
+		return createKunde(vorname, nachname);
+	}
 
+	public static boolean updateKunde(String vornameAlt, String nachnameAlt, String vornameNeu, String nachnameNeu) {
+		return updateKunde(vornameAlt, nachnameAlt, vornameNeu, nachnameNeu);
+	}
 }
--- src/DatabaseUtil/DatabseKunde.java
+++ src/DatabaseUtil/DatabseKunde.java
@@ -24,7 +24,7 @@ public class DatabseKunde {
 		}
 	}
 
-	public static boolean createKunde(String nachname, String vorname) {
+	protected static boolean createKunde(String vorname, String nachname) {
 		PreparedStatement prepS = null;
 		String sql = "INSERT INTO ? (Vorname, Nachname) VALUES('?','?')";
 		boolean success = false;
@@ -32,7 +32,37 @@ public class DatabseKunde {
 			openDatabase();
 			prepS = c.prepareStatement(sql);
 			prepS.setString(1, vorname);
-			prepS.setString(2, vorname);
+			prepS.setString(2, nachname);
+
+			success = prepS.execute();
+			if (prepS != null) {
+				prepS.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (c != null) {
+					c.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return success;
+	}
+
+	protected static boolean updateKunde(String vornameAlt, String nachnameAlt, String vornameNeu, String nachnameNeu) {
+		PreparedStatement prepS = null;
+		String sql = "UPDATE Kunde SET Vorname='?', Nachname='?' WHERE Vorname='?' AND Nachname='?'";
+		boolean success = false;
+		try {
+			openDatabase();
+			prepS = c.prepareStatement(sql);
+			prepS.setString(1, vornameNeu);
+			prepS.setString(2, nachnameNeu);
+			prepS.setString(3, vornameAlt);
+			prepS.setString(4, nachnameAlt);
 
 			success = prepS.execute();
 			if (prepS != null) {
